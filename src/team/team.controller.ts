97b70b3378f994374ad38e59bf0f373e4042d0bf
xhuyz <huylovengoc35@gmail.com>
2025-07-04 14:00:47 +0700

    get teams api

--- src/team/team.controller.ts
+++ src/team/team.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { teamList } from './mock-data';
 
-@Controller('team')
-export class TeamController {}
+@Controller('teams')
+export class TeamController {
+  @Get()
+  getAllTeams() {
+    return {
+      statusCode: 200,
+      message: 'Teams fetched successfully',
+      data: teamList,
+    };
+  }
+}
